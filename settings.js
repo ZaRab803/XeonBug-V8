3ed2189bf84a1a22189552da240161b1d7e3fbe6
Zarab Baloch <136208764+ZaRab803@users.noreply.github.com>
2024-10-20 15:07:05 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,33 +3,33 @@
 //YouTube: @GlobalTechInfo
 //Telegram: https://t.me/GlobalTechInc
 //GitHub: @GlobalTechInfo
-//WhatsApp: https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07
+//WhatsApp: https://whatsapp.com/channel/0029VaShP8AKGGGKhI6C7X0c
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@GlobalTechInfo
 
 const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "923311397803"
+global.ownername = "ZaRab Baloch"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '923311397803'  //creator number
+global.ownername = 'Zarab Baloch' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'GlobalTech\n\nContact: +923444844060'
+global.author = 'ZaRabBaloch'
 
 //console view/theme
 global.themeemoji = '🪀'
 global.wm = "Global Bot"
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.link = 'https://whatsapp.com/channel/0029VaShP8AKGGGKhI6C7X0c'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
@@ -41,7 +41,7 @@ global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti91 = false //auto block +91 
-global.autoswview = false //auto view status/story
+global.autoswview = true //auto view status/story
 
 //menu type 
 //v1 is image menu, 
